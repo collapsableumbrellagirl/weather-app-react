182e66934012cc10cfa1b84d02144371c2231330
Veronica Bueno <desertwillow1947@proton.me>
2024-08-23 21:31:47 -0500

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Search.css";
 
+let apikey = `87b9752c714fbde6317ef3900b3d8fb6`;
+
 function Search({ setWeather }) {
-  const [query, setQuery] = useState(null);
+  const [query, setQuery] = useState();
 
   function handleSubmit(event) {
     event.preventDefault();
 
     let unit = `imperial`;
-    let apikey = `87b9752c714fbde6317ef3900b3d8fb6`;
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit}&appid=${apikey}`;
     axios.get(url).then(function (response) {
       setWeather(response.data);
